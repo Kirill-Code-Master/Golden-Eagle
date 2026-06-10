60de5237d69c9bbad68a21ba4188822de5ca25bf
serufuXX <naehk532@gmail.com>
2026-06-10 04:29:19 +0200

    add test layout and fixed problem with api/https

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import plugin from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
-    server: {
-        port: 53029,
-    }
+  plugins: [plugin()],
+  server: {
+    port: 53029,
+    proxy: {
+      // Любой запрос /api/* будет проксирован на Express-сервер.
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+  },
 })
